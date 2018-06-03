37225bc63cc17935d2f378272cbe330e14fa8300
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2018-06-03 19:28:48 +0900

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCInwvI4hMc5auV0y03G9D6o9JvN4_iiCc',
+    authDomain: 'update-hub.firebaseapp.com',
+    databaseURL: 'https://update-hub.firebaseio.com',
+    projectId: 'update-hub',
+    storageBucket: 'update-hub.appspot.com',
+    messagingSenderId: '899817327474'
+  }
 };
 
 /*
