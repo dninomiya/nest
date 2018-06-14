0e16b70995fa00220b47da5c81bccbd9a014f895
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2018-06-15 02:06:21 +0900

    chore(test): update test files

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -8,7 +8,6 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -25,19 +24,8 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    customLaunchers: {
-      ChromeHeadless: {
-        base: 'Chrome',
-        flags: [
-          '--headless',
-          '--disable-gpu',
-          '--no-sandbox',
-          '--remote-debugging-port=9222',
-        ]
-      }
-    },
-    browsers: ['PhantomJS'],
+    autoWatch: false,
+    browsers: ['Chrome'],
     singleRun: true
   });
 };
