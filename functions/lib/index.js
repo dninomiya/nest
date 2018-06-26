db82683abdaf504232b7a3091953eee072c85045
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2018-06-26 10:32:43 +0900

    chore(git): remove cache

--- functions/lib/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const onCreateUser_1 = require("./onCreateUser");
-exports.onCreateUser = onCreateUser_1.onCreateUser;
-//# sourceMappingURL=index.js.map
